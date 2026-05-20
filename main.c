9c25415e49e316c804511f108f20fc75bf778465
harlandyoga-dev <harlandyoga@gmail.com>
2026-05-20 10:22:51 +0700

    Implement allocation calculation function
    Add function to calculate allocation based on income and family structure.

--- main.c
+++ main.c
@@ -17,6 +17,15 @@ void hitungRasio(float* pendapatan, Rasio* rasio, int bulan){
     }
 }
 
+void hitungAlokasi(float* pendapatan, Alokasi* alokasi, int bulan, int dewasa, int anak){
+    for(int i = 0; i < bulan; i++){
+        alokasi[i].utama = pendapatan[i] * 1 / (1 + 0.5 * (dewasa - 1) + 0.3 * anak);
+        alokasi[i].dewasa = pendapatan[i] * 0.5 / (1 + 0.5 * (dewasa - 1) + 0.3 * anak);
+        alokasi[i].anak = pendapatan[i] * 0.3 / (1 + 0.5 * (dewasa - 1) + 0.3 * anak);
+    }
+}
+
+
 void grafik(float* skala, int bulan, int max){
     for(int pencacah = 0; pencacah < bulan; pencacah++){
         printf("Bulan ke-%d | ", pencacah + 1);
